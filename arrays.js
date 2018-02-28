3d275cc35d97790d6281987d83d1b03260efce7a
Chris Gatewood <c.gatewood55@gmail.com>
2018-02-27 21:18:39 -0600

    array challenges

--- arrays.js
+++ arrays.js
@@ -10,3 +10,26 @@ if(test === opp){
     console.log("same?", false);
 
 }
+
+
+
+// var animals = ['fish', 'dog', 'cat'];
+// var animalsElement= document.getElementById('animals');
+
+// for(var i=0; i<animals.length; i++){
+//     //console.log("animal", animals[i]);
+// }
+// animalsElement.innerHTML += animals{i};
+
+
+var book= "Catch 22";
+var bookSplit = book.split("");
+var realNumz =[];
+for(var j=0; j<bookSplit.length; j++){
+    if(bookSplit[j]*1){
+    realNumz.push(bookSplit[j]);
+    
+}
+}
+console.log("answer", realNumz.join(""))
+
