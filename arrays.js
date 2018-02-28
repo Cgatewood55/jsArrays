d71cd1b69a3a10013e3f1e521d8dc08f1c35e6cd
Chris Gatewood <c.gatewood55@gmail.com>
2018-02-27 19:48:48 -0600

    arrayschallenge

--- arrays.js
+++ arrays.js
@@ -1,1 +1,12 @@
 console.log('arrays.js');
+
+var test = "cats";
+var opp = test.split("").reverse().join("");
+
+if(test === opp){
+    console.log("same?", true);
+
+} else {
+    console.log("same?", false);
+
+}
